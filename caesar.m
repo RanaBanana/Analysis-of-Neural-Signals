03066e2980aeaf7199f350aaf2ac968617c69e4e
ElineHa <70947715+ElineHa@users.noreply.github.com>
2020-09-15 11:44:05 +0200

    Update caesar.m

--- caesar.m
+++ caesar.m
@@ -5,7 +5,8 @@ function [v] = caesar(x,m)
 % m = amount of positions characters are rotated to the right.  
 
 %Converting the input to upper case letters (this happens inside the function
-%letterToNumber) and 
+%letterToNumber) and assigning letters to the new numbers (resulting from 
+% x+shift) within the Z26 (which is done by the mod function).
 x = letterToNumber(x); 
 new_letters= mod(x+m,26);
 v = numberToLetter(new_letters);
