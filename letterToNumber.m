b66da9db8a5c6f4b4935a18ef18ba9590c5c0c1c
ElineHa <70947715+ElineHa@users.noreply.github.com>
2020-09-16 21:34:20 +0200

    Update letterToNumber.m

--- letterToNumber.m
+++ letterToNumber.m
@@ -1,6 +1,13 @@
 function [v] = letterToNumber(x) 
-% lower case a start at 97
+% This function converts a letter to a number within Z26.
+% Input: a (number of) letter(s) (could also be a lowercase letter)
+% Output: the corresponding number (s)
+
+% Control for non-capitalised letters
 x = upper(x);
+
+% This if-statement controls for numeric input by excluding it from the
+% statement.
 if isnumeric(x)==false;
     v = double(char(x)) -65;
 end
