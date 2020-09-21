8d71e78627c7172141c9fb78313ecb5c2be7f0ac
ElineHa <70947715+ElineHa@users.noreply.github.com>
2020-09-21 17:21:58 +0200

    Add files via upload

--- multinverse.m
+++ multinverse.m
@@ -3,10 +3,12 @@ function [c, ModMultInv] = multinverse(n,m)
 % multiplicative inverse in Zm for any given modulus m. 
 % Input: natural number n and a parameter m (for the modulus)
 % Output: True or False based on whether the number has a mult.inv in Zm.
+% Output2: the modulus of the multiplicative inverse.
 
 n = abs(round(n));
 
-[G, C] = gcd(n,m);
+% G = the greatest common divisor; C = multiplicative inverse
+[G, C] = gcd(n,m); 
 if G==1         % The inverse of n(mod m) only exists if gcd(n,m)=1
     ModMultInv = mod(C,m);
     c = true;
