421eb27aa452d1987f37e34dddf8392f625f7a6b
ElineHa <70947715+ElineHa@users.noreply.github.com>
2020-09-22 10:25:24 +0200

    Update caesar.m

--- caesar.m
+++ caesar.m
@@ -1,12 +1,12 @@
 function [v_en, v_de] = caesar(x,m) 
-%This function encrypts and decrypts a plaintext message (x) by using caesar's
-%cypher. 
+% This function encrypts and decrypts a plaintext message (x) by using caesar's
+% cypher. 
 % x = input message
 % m = amount of positions characters are rotated to the right.  
 
-%Converting the input to upper case letters (this happens inside the function
-%letterToNumber) and assigning letters to the new numbers (resulting from 
-% x+shift) within the Z26 (which is done by the mod function).
+% Convert the input to upper case letters (this happens inside the function
+% letterToNumber) and assigning letters to the new numbers (resulting from 
+% x+shift) within Z26 (which is done by the mod function).
 x = letterToNumber(x); 
 new_letters= mod(x+m,26);
 v_en = numberToLetter(new_letters);
